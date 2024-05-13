2de02de1d2d0331dd6a5410f3095564c2ef46bf5
[SW8] 정영준 <wjddudwns567@gmail.com>
2024-05-13 23:39:16 +0900

    FIX: 구글애널릭틱스 문제

--- src/app/googleAnalytics.tsx
+++ src/app/googleAnalytics.tsx
@@ -1,40 +1,25 @@
-"use client";
-
-import { pageview } from "@/lib/gtag";
 import Script from "next/script";
-import { useEffect } from "react";
-import { usePathname } from "next/navigation";
 
-function GoogleAnalytics({ GA_TRACKING_ID }: { GA_TRACKING_ID?: string }) {
-  const pathname = usePathname();
-
-  useEffect(() => {
-    if (pathname) {
-      pageview(pathname);
-    }
-  }, [pathname]);
-
-  if (process.env.NODE_ENV !== "production") {
-    return null;
-  }
-
+export default function GoogleAnalytics({ gaId }: { gaId: string }) {
   return (
     <>
       <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        async
+        src={`https://www.googletagmanager.com/gtag/js
+				?id=${gaId}`}
       />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
- 
-          gtag('config', '${GA_TRACKING_ID}');
-        `}
-      </Script>
+      <Script
+        id="google-analytics"
+        dangerouslySetInnerHTML={{
+          __html: `
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', '${gaId}');
+		`,
+        }}
+      />
     </>
   );
 }
-
-export default GoogleAnalytics;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,8 @@ import { Metadata } from "next";
 import GoogleAnalytics from "./googleAnalytics";
 
 export const metadata: Metadata = {
-  title: "Fastcampus NextMap",
+  title: "NextMap",
   description: "Next.js 13을 이용한 맛집 앱",
-  openGraph: {
-    title: "Fastcampus NextMap",
-    description: "Next.js 13을 이용한 맛집 앱",
-  },
 };
 
 export default function RootLayout({
@@ -20,8 +16,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <GoogleAnalytics GA_TRACKING_ID={process.env.NEXT_PUBLIC_GA_ID} />
         <NextProvider>
+          {process.env.NEXT_PUBLIC_GA_ID ? (
+            <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
+          ) : null}
           <NextLayout>{children}</NextLayout>
         </NextProvider>
       </body>
