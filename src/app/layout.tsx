0aeda1c9e6fd6474e329e082659a6640190e6e32
[SW8] 정영준 <wjddudwns567@gmail.com>
2024-05-13 23:54:07 +0900

    Fix: 최종 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import { NextProvider, NextLayout } from "./providers";
 import { Metadata } from "next";
-import { GoogleAnalytics } from "@next/third-parties/google";
+import GoogleAnalytics from "./googleAnalytics";
 
 export const metadata: Metadata = {
   title: "NextMap",
